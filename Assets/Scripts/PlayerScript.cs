c452b9f10fa9672184832d13677b1289b91577dc
hiralshukla <hiralshukla365@gmail.com>
2026-01-17 22:53:14 -0500

    feat: health bar sliders

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth;
+    public int maxGlow;
+    public int currentGlow; 
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
